c00736a4a179316bb2d48466a3f996f138e29535
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-18 00:41:02 +0100

    querySelector

--- app.js
+++ app.js
@@ -3,11 +3,23 @@
 console.log(location)
 // console.log(window.location)
 
-console.log(setTimeout(console.log, 3000))
+// console.log(setTimeout(console.log, 3000))
 // console.log(window.setTimeout(console.log, 3000))
 
 // générer une variable globale (passer info d'un script à un autre)
-window.a = 3
+// window.a = 3
 
 // n'exécute pas le script avant la fin de l'alerte
-alert("DANGER")
+// alert("DANGER")
+
+
+// MANIPULER LE DOM
+
+console.log(document.querySelector("#hello"))
+
+console.log(document.querySelectorAll("li"))
+
+const lis = document.querySelectorAll("li")
+console.log(lis[1])
+lis.forEach((v, i) => { 
+    console.log(v, i)})
