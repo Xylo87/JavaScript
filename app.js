272a55a81f3abc4055267537fbdad11d9411ba70
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-19 00:21:10 +0100

    toggle

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // JAVASCRIPT NAVIGATEUR
 
-console.log(location)
+// console.log(location)
 // console.log(window.location)
 
 // console.log(setTimeout(console.log, 3000))
@@ -15,11 +15,46 @@ console.log(location)
 
 // MANIPULER LE DOM
 
-console.log(document.querySelector("#hello"))
+// console.log(document.querySelector("#hello"))
 
-console.log(document.querySelectorAll("li"))
+// console.log(document.querySelectorAll("li"))
 
-const lis = document.querySelectorAll("li")
-console.log(lis[1])
-lis.forEach((v, i) => { 
-    console.log(v, i)})
+// const lis = document.querySelectorAll("li")
+// console.log(lis[1])
+// lis.forEach((v, i) => { 
+//     console.log(v, i)})
+
+const ul = document.querySelector("ul li:first-of-type")
+// console.log(
+//     ul.nodeName,
+//     // nom du noeud HTML sur lequel on se trouve > en MAJ
+
+//     ul.innerHTML,
+//     // récupérer structure HTML de l'élément > peut être modifié si redéclaré >
+//     // > ul.innerHTML = "hello"
+//     // console.log(ul.innerHTML)
+
+//     ul.innerText,
+//     // récupérer texte sans structure HTML > modifiable également
+
+//     ul.textContent,
+//     // récupérer texte en respecter simplement les espaces de la structure HTML + éléments cachés + JavaScript
+
+//     ul.setAttribute("hidden", "hidden"),
+//     // remplacer un attribut par un autre
+
+//     ul.removeAttribute("hidden"))
+
+// console.log(
+//     ul.getAttribute("class")
+// )
+// récupérer l'attribut d'un élément
+
+// console.log(
+//     ul.classList.remove("red")
+// )
+// lister/enlever une classe à un élément
+
+// setInterval(() => {
+//     ul.classList.toggle("red")
+// }, 1000)
