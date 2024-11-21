9733a25aa5d0b4823a72d1deadb66a56a05782d2
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-21 21:56:04 +0100

    CounterAppPractice

--- Practice/app.js
+++ Practice/app.js
@@ -3,9 +3,11 @@
 
 let countEl = document.getElementById("count-el")
 let button = document.querySelector("button")
+let saveBtn = document.getElementById("save-btn")
 
 console.log(countEl)
 console.log(button)
+console.log(saveBtn)
 
 let count = 0
 
@@ -19,3 +21,23 @@ button.addEventListener("click", function increment() {
     countEl.innerText = count
     console.log(count)
 })
+
+saveBtn.addEventListener("click", function save() {
+    // count = 0
+    // countEl.innerText = count
+    console.log(count)
+})
+
+
+
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// console.log(welcomeEl)
+
+// let name = "Théo"
+// let greeting = "Welcome back "
+
+// welcomeEl.innerText = greeting + name
+
+// welcomeEl.innerText += " ⚡"
