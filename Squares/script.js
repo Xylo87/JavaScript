5b331c894851b5602e76ca8f7415663b32451fc0
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-22 11:01:42 +0100

    files+SquarePractice

---
+++ Squares/script.js
@@ -0,0 +1,14 @@
+const square = document.querySelector(".square")
+
+console.log(square)
+
+square.addEventListener("click", function CSSDisplay() {
+    alert(
+        " - Class : " + square.getAttribute("class") + "\n" +
+        " - Color : " + getComputedStyle(square).color + "\n" +
+        " - Background color : " + getComputedStyle(square).background + "\n" +
+        " - Height : " + getComputedStyle(square).height + "\n" +
+        " - Width : " + getComputedStyle(square).width + "\n" +
+        " - Font-family : " + getComputedStyle(square).fontFamily + "\n" +
+        " - Font-size : " + getComputedStyle(square).fontSize)
+})
