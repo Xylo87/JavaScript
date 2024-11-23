3886046bcd7e58761807f165c445189c94edc60a
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-23 12:34:27 +0100

    exo2Save

--- Squares/script.js
+++ Squares/script.js
@@ -1,12 +1,13 @@
-const square = document.querySelector(".square")
+// const square = document.querySelector(".square")
 
-square.addEventListener("click", function CSSDisplay() {
-    alert(
-        " - Class : " + square.getAttribute("class") + "\n" +
-        " - Color : " + getComputedStyle(square).color + "\n" +
-        " - Background color : " + getComputedStyle(square).background + "\n" +
-        " - Height : " + getComputedStyle(square).height + "\n" +
-        " - Width : " + getComputedStyle(square).width + "\n" +
-        " - Font-family : " + getComputedStyle(square).fontFamily + "\n" +
-        " - Font-size : " + getComputedStyle(square).fontSize)
-})
+// square.addEventListener("click", function CSSDisplay() {
+//     alert(
+//         " - Class : " + square.getAttribute("class") + "\n" +
+//         " - Color : " + getComputedStyle(square).color + "\n" +
+//         " - Background color : " + getComputedStyle(square).background + "\n" +
+//         " - Height : " + getComputedStyle(square).height + "\n" +
+//         " - Width : " + getComputedStyle(square).width + "\n" +
+//         " - Font-family : " + getComputedStyle(square).fontFamily + "\n" +
+//         " - Font-size : " + getComputedStyle(square).fontSize)
+// })
+
