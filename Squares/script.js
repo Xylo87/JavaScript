b246b17bc0d8cdec71ec19a8c5835d5805869f32
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-24 00:43:01 +0100

    working

--- Squares/script.js
+++ Squares/script.js
@@ -11,3 +11,46 @@
 //         " - Font-size : " + getComputedStyle(square).fontSize)
 // })
 
+
+const squareWP = document.querySelector(".squareWP")
+const squareDE = document.querySelector(".squareDE")
+const squareSNW = document.querySelector(".squareSNW")
+const squareKNY = document.querySelector(".squareKNY")
+const squareO = document.querySelector(".squareO")
+
+console.log(squareWP, squareDE,squareSNW, squareKNY, squareO)
+
+
+
+
+squareWP.addEventListener("click", function colorchange() {
+    squareO.removeAttribute("class")
+    squareO.classList.add("squareWP")
+    squareO.innerHTML = getComputedStyle(squareWP).background
+})
+
+squareDE.addEventListener("click", function colorchange() {
+    squareO.removeAttribute("class")
+    squareO.classList.add("squareDE")
+    squareO.innerHTML = getComputedStyle(squareDE).background
+})
+
+squareSNW.addEventListener("click", function colorchange() {
+    squareO.removeAttribute("class")
+    squareO.classList.add("squareSNW")
+    squareO.innerHTML = getComputedStyle(squareSNW).background
+})
+
+squareKNY.addEventListener("click", function colorchange() {
+    squareO.removeAttribute("class")
+    squareO.classList.add("squareKNY")
+    squareO.innerHTML = getComputedStyle(squareKNY).background
+})
+
+
+
+
+// function colorchange() {
+//     
+// }
+
