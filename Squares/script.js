6aff9934e8c35e28429beca5f47e82ecfb1ad710
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-24 23:20:30 +0100

    realAllClear

--- Squares/script.js
+++ Squares/script.js
@@ -1,32 +1,3 @@
-
-
-// METHODE MOYENNE ???
-
-
-// const squareWP = document.querySelector("#squareWP")
-// const squareDE = document.querySelector("#squareDE")
-// const squareSNW = document.querySelector("#squareSNW")
-// const squareKNY = document.querySelector("#squareKNY")
-// const squareO = document.querySelector("#squareO")
-
-
-// squareWP.addEventListener("click", colorchange(squareWP))
-// squareDE.addEventListener("click", colorchange(squareDE))
-// squareSNW.addEventListener("click", colorchange(squareSNW))
-// squareKNY.addEventListener("click", colorchange(squareKNY))
-
-
-// function colorchange(carre) {
-//     squareO.setAttribute("id", carre.id)
-//     squareO.innerHTML = getComputedStyle(carre).background
-// }
-
-
-
-
-// METHODE COURTE
-
-
 document.querySelectorAll(".square").forEach(carre => {
     carre.addEventListener("click", function rollcolor() {
         carre.classList.toggle("redroll")
