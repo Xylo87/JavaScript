a184fc4a79bd84f6bb1f720b3774f83a338f9491
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-24 13:38:50 +0100

    optiTests

--- Squares/script.js
+++ Squares/script.js
@@ -12,38 +12,31 @@
 // })
 
 
-const squareWP = document.querySelector(".squareWP")
-const squareDE = document.querySelector(".squareDE")
-const squareSNW = document.querySelector(".squareSNW")
-const squareKNY = document.querySelector(".squareKNY")
-const squareO = document.querySelector(".squareO")
-
-console.log(squareWP, squareDE,squareSNW, squareKNY, squareO)
-
+const squareWP = document.querySelector("#squareWP")
+const squareDE = document.querySelector("#squareDE")
+const squareSNW = document.querySelector("#squareSNW")
+const squareKNY = document.querySelector("#squareKNY")
+const squareO = document.querySelector("#squareO")
 
 
 
 squareWP.addEventListener("click", function colorchange() {
-    squareO.removeAttribute("class")
-    squareO.classList.add("squareWP")
+    squareO.setAttribute("id", "squareWP")
     squareO.innerHTML = getComputedStyle(squareWP).background
 })
 
 squareDE.addEventListener("click", function colorchange() {
-    squareO.removeAttribute("class")
-    squareO.classList.add("squareDE")
+    squareO.setAttribute("id", "squareDE")
     squareO.innerHTML = getComputedStyle(squareDE).background
 })
 
 squareSNW.addEventListener("click", function colorchange() {
-    squareO.removeAttribute("class")
-    squareO.classList.add("squareSNW")
+    squareO.setAttribute("id", "squareSNW")
     squareO.innerHTML = getComputedStyle(squareSNW).background
 })
 
 squareKNY.addEventListener("click", function colorchange() {
-    squareO.removeAttribute("class")
-    squareO.classList.add("squareKNY")
+    squareO.setAttribute("id", "squareKNY")
     squareO.innerHTML = getComputedStyle(squareKNY).background
 })
 
@@ -53,4 +46,7 @@ squareKNY.addEventListener("click", function colorchange() {
 // function colorchange() {
 //     
 // }
+
+
+
 
