a5241325ad9fe665d384abfda7c1e3c04dfcf6ab
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-24 18:20:21 +0100

    long+shortOpti

--- Squares/script.js
+++ Squares/script.js
@@ -1,52 +1,73 @@
-// const square = document.querySelector(".square")
 
-// square.addEventListener("click", function CSSDisplay() {
-//     alert(
-//         " - Class : " + square.getAttribute("class") + "\n" +
-//         " - Color : " + getComputedStyle(square).color + "\n" +
-//         " - Background color : " + getComputedStyle(square).background + "\n" +
-//         " - Height : " + getComputedStyle(square).height + "\n" +
-//         " - Width : " + getComputedStyle(square).width + "\n" +
-//         " - Font-family : " + getComputedStyle(square).fontFamily + "\n" +
-//         " - Font-size : " + getComputedStyle(square).fontSize)
+
+
+// METHODE LONGUE
+
+
+// const squareWP = document.querySelector("#squareWP")
+// const squareDE = document.querySelector("#squareDE")
+// const squareSNW = document.querySelector("#squareSNW")
+// const squareKNY = document.querySelector("#squareKNY")
+// const squareO = document.querySelector("#squareO")
+
+
+// squareWP.addEventListener("click", function colorchange() {
+//     squareO.setAttribute("id", "squareWP")
+//     squareO.innerHTML = getComputedStyle(squareWP).background
+// })
+
+// squareDE.addEventListener("click", function colorchange() {
+//     squareO.setAttribute("id", "squareDE")
+//     squareO.innerHTML = getComputedStyle(squareDE).background
+// })
+
+// squareSNW.addEventListener("click", function colorchange() {
+//     squareO.setAttribute("id", "squareSNW")
+//     squareO.innerHTML = getComputedStyle(squareSNW).background
+// })
+
+// squareKNY.addEventListener("click", function colorchange() {
+//     squareO.setAttribute("id", "squareKNY")
+//     squareO.innerHTML = getComputedStyle(squareKNY).background
 // })
 
 
-const squareWP = document.querySelector("#squareWP")
-const squareDE = document.querySelector("#squareDE")
-const squareSNW = document.querySelector("#squareSNW")
-const squareKNY = document.querySelector("#squareKNY")
+
+// METHODE MOYENNE ???
+
+
+// const squareWP = document.querySelector("#squareWP")
+// const squareDE = document.querySelector("#squareDE")
+// const squareSNW = document.querySelector("#squareSNW")
+// const squareKNY = document.querySelector("#squareKNY")
+// const squareO = document.querySelector("#squareO")
+
+
+// squareWP.addEventListener("click", colorchange(squareWP))
+// squareDE.addEventListener("click", colorchange(squareDE))
+// squareSNW.addEventListener("click", colorchange(squareSNW))
+// squareKNY.addEventListener("click", colorchange(squareKNY))
+
+
+// function colorchange(carre) {
+//     squareO.setAttribute("id", carre.id)
+//     squareO.innerHTML = getComputedStyle(carre).background
+// }
+
+
+
+// METHODE COURTE
+
+
 const squareO = document.querySelector("#squareO")
 
 
-
-squareWP.addEventListener("click", function colorchange() {
-    squareO.setAttribute("id", "squareWP")
-    squareO.innerHTML = getComputedStyle(squareWP).background
+document.querySelectorAll(".square").forEach(carre => {
+    carre.addEventListener("click", function colorchange(forme) {
+         squareO.setAttribute("id", carre.id)
+        squareO.innerHTML = getComputedStyle(carre).background
+    })
 })
-
-squareDE.addEventListener("click", function colorchange() {
-    squareO.setAttribute("id", "squareDE")
-    squareO.innerHTML = getComputedStyle(squareDE).background
-})
-
-squareSNW.addEventListener("click", function colorchange() {
-    squareO.setAttribute("id", "squareSNW")
-    squareO.innerHTML = getComputedStyle(squareSNW).background
-})
-
-squareKNY.addEventListener("click", function colorchange() {
-    squareO.setAttribute("id", "squareKNY")
-    squareO.innerHTML = getComputedStyle(squareKNY).background
-})
-
-
-
-
-// function colorchange() {
-//     
-// }
-
 
 
 
