52dd6d2f8d4a8dfed39e2226e1445d396b7878be
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-24 22:08:22 +0100

    exo3Done

--- Squares/script.js
+++ Squares/script.js
@@ -1,38 +1,5 @@
 
 
-
-// METHODE LONGUE
-
-
-// const squareWP = document.querySelector("#squareWP")
-// const squareDE = document.querySelector("#squareDE")
-// const squareSNW = document.querySelector("#squareSNW")
-// const squareKNY = document.querySelector("#squareKNY")
-// const squareO = document.querySelector("#squareO")
-
-
-// squareWP.addEventListener("click", function colorchange() {
-//     squareO.setAttribute("id", "squareWP")
-//     squareO.innerHTML = getComputedStyle(squareWP).background
-// })
-
-// squareDE.addEventListener("click", function colorchange() {
-//     squareO.setAttribute("id", "squareDE")
-//     squareO.innerHTML = getComputedStyle(squareDE).background
-// })
-
-// squareSNW.addEventListener("click", function colorchange() {
-//     squareO.setAttribute("id", "squareSNW")
-//     squareO.innerHTML = getComputedStyle(squareSNW).background
-// })
-
-// squareKNY.addEventListener("click", function colorchange() {
-//     squareO.setAttribute("id", "squareKNY")
-//     squareO.innerHTML = getComputedStyle(squareKNY).background
-// })
-
-
-
 // METHODE MOYENNE ???
 
 
@@ -56,18 +23,12 @@
 
 
 
+
 // METHODE COURTE
 
 
-// const squareO = document.querySelector("#squareO")
-
-
-// document.querySelectorAll(".square").forEach(carre => {
-//     carre.addEventListener("click", function colorchange(forme) {
-//         squareO.setAttribute("id", carre.id)
-//         squareO.innerHTML = getComputedStyle(carre).background
-//     })
-// })
-
-
-
+document.querySelectorAll(".square").forEach(carre => {
+    carre.addEventListener("click", function rollcolor() {
+        carre.classList.toggle("redroll")
+    })
+});
