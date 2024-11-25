7127d82ac5ca49cca2cf2d08b58e994aecf22275
Xylo <theo.arbogast@hotmail.fr>
2024-11-25 12:05:01 +0100

    stillGoing

--- Squares/script.js
+++ Squares/script.js
@@ -1,5 +1,22 @@
+// document.querySelectorAll(".square").forEach(carre => {
+//     carre.addEventListener("click", function rollcolor() {
+//         carre.classList.toggle("redroll")
+//     })
+// });
+
+
+const html = document.querySelector("html")
+
+
 document.querySelectorAll(".square").forEach(carre => {
-    carre.addEventListener("click", function rollcolor() {
-        carre.classList.toggle("redroll")
+    carre.addEventListener("click", function () {
+        carre.classList.toggle("box")
     })
 });
+
+
+// test.addEventListener("click", function test2() {
+//     // html.style.background = 
+//     test.classList.toggle("box")
+//     // .classList.toggle("")
+// })
