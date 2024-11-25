8fdc6dc0ad4402da675535e356450e3bc8f268b7
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-25 19:46:18 +0100

    save

--- Squares/script.js
+++ Squares/script.js
@@ -5,18 +5,47 @@ document.querySelectorAll(".square").forEach(carre => {
     const socialName = carre.querySelector("p")
 
     carre.addEventListener("click", function socialmedia() {
-        carre.classList.toggle("box")
-        socialName.classList.toggle("display")
         if (carre.classList.contains("box")) {
-            body.style.background = getComputedStyle(carre).background
-        }   else {
+            carre.classList.remove("box")
             body.style.background = "grey"
+            socialName.classList.remove("display")
+            return
         }
+        
+        document.querySelectorAll(".square").forEach(c => {
+            c.classList.remove("box")
+            c.querySelector("p").classList.remove("display")
+        })
+
+        carre.classList.add("box")
+        socialName.classList.add("display")
+        body.style.background = getComputedStyle(carre).background
     })
 });
 
 // body.style.background = carre.dataset.color
 
-// carres.forEach((c) => {
-        //     c.classList.remove('box')
-        // })
+
+
+
+
+
+
+// const body = document.querySelector("body")
+
+// document.querySelectorAll(".square").forEach(carre => {
+
+//     const socialName = carre.querySelector("p")
+
+//     carre.addEventListener("click", function socialmedia() {
+//         carre.classList.toggle("box")
+//         socialName.classList.toggle("display")
+//         if (carre.classList.contains("box")) {
+//             body.style.background = getComputedStyle(carre).background
+//         }   else {
+//             body.style.background = "grey"
+//         }
+//     })
+// });
+
+// body.style.background = carre.dataset.color
