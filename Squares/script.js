e1014d74b588fecf68eaf5c6d2409fcf23eee77a
Xylo <theo.arbogast@hotmail.fr>
2024-11-25 09:30:07 +0100

    exo1reDone

--- Squares/script.js
+++ Squares/script.js
@@ -2,10 +2,10 @@ const carre = document.querySelector(".square")
 
 carre.addEventListener("click", function CSSdisplay() {
     alert("Class : " + carre.getAttribute("class") + "\n" + 
-    "Class : " + carre.getAttribute("class") + "\n" 
-
-
-
-
-)
+    "Background color : " + getComputedStyle(carre).background + "\n" +
+    "Color : " + getComputedStyle(carre).color + "\n" +
+    "Height : " + getComputedStyle(carre).height + "\n" +
+    "Width : " + getComputedStyle(carre).width + "\n" +
+    "Font-family : " + getComputedStyle(carre).fontFamily + "\n" +
+    "Font-size : " + getComputedStyle(carre).fontSize + "\n")
 })
