5a2773bb8fa36d134e3452a42be693f52f5e8cca
Xylo87 <theo.arbogast@hotmail.fr>
2025-01-23 23:14:23 +0100

    feat/update : pratique de la méthode innerHTML

--- Practice/Extension/script.js
+++ Practice/Extension/script.js
@@ -3,14 +3,26 @@ const button = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 let myLeads = ["Kiouze", "Kazel", "Isimorn", "Xylo"]
 
-button.addEventListener("click", function () {
-    // myLeads.push(inputText.value)
-    myLeads.forEach(element => {
-        // ulEl.textContent += element + " "
-        ulEl.innerHTML += "<li>" + element + "</li>"
-    })
+// button.addEventListener("click", function () {
+//     // myLeads.push(inputText.value)
+//     myLeads.forEach(element => {
+//         // ulEl.textContent += element + " "
+//         ulEl.innerHTML += "<li>" + element + "</li>"
+//     })
     // for (let i = 0; i < myLeads.length; i++) {
     //     console.log(myLeads[i]);
     // }
+// })
+
+
+
+
+
+const test = document.getElementById("container")
+test.innerHTML = "<button id=\"testButton\">Buy !</button><br><br>"
+
+const test2 = document.getElementById("testButton")
+test2.addEventListener("click", function () {
+    test.innerHTML += "<p>Thank you for buying !</p>"
 })
 
