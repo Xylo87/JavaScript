522c48314d8b3712287dce8b9dc6ad299a731ad6
Xylo87 <theo.arbogast@hotmail.fr>
2025-01-23 22:40:12 +0100

    update : modifications pour une optimisation de rendu à chaque chargement

--- Practice/Extension/script.js
+++ Practice/Extension/script.js
@@ -1,13 +1,16 @@
 const inputText = document.getElementById("input-el")
 const button = document.getElementById("input-btn")
-let myLeads = ["kiouze", "kazel", "isimorn", "xylo"]
+const ulEl = document.getElementById("ul-el")
+let myLeads = ["Kiouze", "Kazel", "Isimorn", "Xylo"]
 
 button.addEventListener("click", function () {
     // myLeads.push(inputText.value)
     myLeads.forEach(element => {
-        console.log(element)
+        // ulEl.textContent += element + " "
+        ulEl.innerHTML += "<li>" + element + "</li>"
     })
     // for (let i = 0; i < myLeads.length; i++) {
     //     console.log(myLeads[i]);
     // }
 })
+
