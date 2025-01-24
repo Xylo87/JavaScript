f792697a73451262f3a92cc509d6890b61201d4d
Théo <theo.arbogast@hotmail.fr>
2025-01-24 16:44:38 +0100

    feat + update : extension fonctionnel (sauvegarde de liens HTTP) + préparation au déploiement

--- Practice/Extension/script.js
+++ Practice/Extension/script.js
@@ -1,28 +1,68 @@
+const container = document.getElementById("container")
 const inputText = document.getElementById("input-el")
 const button = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let myLeads = ["Kiouze", "Kazel", "Isimorn", "Xylo"]
 
-// button.addEventListener("click", function () {
-//     // myLeads.push(inputText.value)
-//     myLeads.forEach(element => {
-//         // ulEl.textContent += element + " "
-//         ulEl.innerHTML += "<li>" + element + "</li>"
-//     })
-    // for (let i = 0; i < myLeads.length; i++) {
-    //     console.log(myLeads[i]);
-    // }
-// })
+let myLeads = []
+
+const errorCanva = document.createElement("p")
+errorCanva.textContent = "Please enter a valid URL"
 
 
 
+function renderLeads() {
+    let listItems = ""
+    
+    myLeads.forEach(element => {
+        listItems += 
+        `<li>
+            <a href='${element}' target='_blank'> 
+                ${element}
+            </a>
+        </li>`
+        
+        ulEl.innerHTML = listItems
+    })
+}
 
 
-const test = document.getElementById("container")
-test.innerHTML = "<button id=\"testButton\">Buy !</button><br><br>"
 
-const test2 = document.getElementById("testButton")
-test2.addEventListener("click", function () {
-    test.innerHTML += "<p>Thank you for buying !</p>"
+button.addEventListener("click", function () {
+    let text = inputText.value
+    
+    // if (inputText.value === "") return
+    if (text.startsWith("http")) {
+        
+        errorCanva.remove()
+
+        myLeads.push(inputText.value)
+        renderLeads()
+        
+        inputText.value = ""
+    } else {
+        container.append(errorCanva)
+    }
 })
 
+
+
+// for (let i = 0; i < myLeads.length; i++) {
+//     console.log(myLeads[i]);
+// }
+
+// test.innerHTML = "<button id=\"testButton\">Buy !</button><br><br>"
+
+
+
+// const recipient = "James"
+// const sender =  "Théo"
+
+// const email = "Hey " + recipient + " ! How is it going ? Cheers Per"
+
+// console.log(email)
+
+// const email2 = `Hey ${recipient} ! 
+// How is it going ? 
+// Cheers ${sender}`
+
+// console.log(email2)
